ba56d69fe547fac47466d9875d4dbbe9dbb84d88
joycemolly <zhenq438@newschool.edu>
2017-12-07 23:51:04 -0500

    ball bouce when touching mouse
    ball bouce to the oppsite angle when touching mouse position

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -2,17 +2,47 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
+    ofBackground(0);//backgroundcolor
+    ballPos.set(ofRandom(ofGetWidth()), ofRandom(ofGetHeight()));//ball's firstrandom pos
+    vel.set(5,5);//ball's velocity
 
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
-
+    ballPos+=vel;//ball move
+    dis = ofPoint(ofGetMouseX(),ofGetMouseY())-ofPoint(ballPos.x,ballPos.y);//distance between mouse and ball
+    
+    if(dis.length()<100){
+        vel*=-1;
+    }//bounce when touching the mouse
+    
+    if(ballPos.x>ofGetWidth()-30){
+        ballPos.x=ofGetWidth()-30;
+        vel.x*=-1;
+    };
+    if(ballPos.y>ofGetHeight()-30){
+        ballPos.y=ofGetHeight()-30;
+        vel.y*=-1;
+    }
+    if(ballPos.x<30){
+        ballPos.x =30;
+        vel.x*=-1;
+    }
+    if(ballPos.y<30){
+        ballPos.y=30;
+        vel.y*=-1;
+    }///bouce when touching wall
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-
+    ofFill();
+    ofDrawCircle(ballPos, 30);
+    ofNoFill();
+    ofDrawCircle(ofGetMouseX(),ofGetMouseY(),50);
+    
 }
 
 //--------------------------------------------------------------
